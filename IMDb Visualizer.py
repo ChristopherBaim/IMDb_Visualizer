e180e0d93fa93bb43dc3c4498b53c7f4cceb9960
yoyochris924 <35181352+yoyochris924@users.noreply.github.com>
2020-11-09 22:50:47 -0500

    Updated search function to prevent repeated/empty searches. Condensed separate callbacks into one multi-output callback.

--- IMDb Visualizer.py
+++ IMDb Visualizer.py
@@ -5,69 +5,72 @@ import plotly.graph_objs as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 #### Gets IMDb ID from show name
 
 
 def singleShow(rawName):
     #rawName = input("Show name:")
-    showName = rawName.replace(' ', '+')
-    url = 'http://www.imdb.com/search/title?title=' + showName + '&title_type=tv_series'
+    if rawName != None:
+        showName = rawName.replace(' ', '+')
+        url = 'http://www.imdb.com/search/title?title=' + showName + '&title_type=tv_series'
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
 
-    show = soup.find(class_='lister-item-header')
+        show = soup.find(class_='lister-item-header')
 
-    href = str(show.find('a'))
-    # print(href)
-
-    #### Confirm correct show name
-    print(show.find('a').text)
-    imdbID = href[16:25]
-    print(imdbID)
-
-    url2 = 'http://www.imdb.com/title/' + imdbID + '/ratings?ref_=tt_ov_rt'
-
-    page = requests.get(url2)
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    rt = soup.find_all(class_='ratingTable')
-
-    demo = []
-    rating = []
-    votes = []
-    wanted = ['imdb_users', 'males', 'females']
-
-    #####Finds rating and number of voters for listed demographics
-    for x in range(0, len(rt)-5):
-        href = str(rt[x].find('a'))
-        index = href.find("=", 10) + 1
-        index2 = href.find(">", 10) - 1
-
-        if href[index:index2] in wanted:
-            demo.append(href[index:index2])
-            rating.append(rt[x].find(class_='bigcell').text)
-            votes.append(str(rt[x].find('a').text).strip())
+        if show == None:
+            print("No show found")
+        else:
+            href = str(show.find('a'))
 
 
-    fig = go.Figure()
-    fig.add_trace(go.Bar(
-        x= demo,
-        y = rating,
-        marker_color = ['darkslateblue', 'cornflowerblue', 'hotpink'],
-        text = rating,
-        textposition = 'auto'))
+            #### Confirm correct show name
+            cleanName = show.find('a').text
+            print(cleanName)
+            imdbID = href[16:25]
+            print(imdbID)
+
+            url2 = 'http://www.imdb.com/title/' + imdbID + '/ratings?ref_=tt_ov_rt'
+
+            page = requests.get(url2)
+            soup = BeautifulSoup(page.text, 'html.parser')
+
+            rt = soup.find_all(class_='ratingTable')
+
+            demo = []
+            rating = []
+            votes = []
+            wanted = ['imdb_users', 'males', 'females']
+
+            #####Finds rating and number of voters for listed demographics
+            for x in range(0, len(rt)-5):
+                href = str(rt[x].find('a'))
+                index = href.find("=", 10) + 1
+                index2 = href.find(">", 10) - 1
+
+                if href[index:index2] in wanted:
+                    demo.append(href[index:index2])
+                    rating.append(rt[x].find(class_='bigcell').text)
+                    votes.append(str(rt[x].find('a').text).strip())
 
 
-    fig.update_layout(
-        title='IMDb Ratings by Demographic')
-    return fig
-#pyo.plot(fig, filename='IMDb.html')
-####delete if not separate method
+            fig = go.Figure()
+            fig.add_trace(go.Bar(
+                x= demo,
+                y = rating,
+                marker_color = ['darkslateblue', 'cornflowerblue', 'hotpink'],
+                text = rating,
+                textposition = 'auto'))
+
+
+            fig.update_layout(
+                title='IMDb Ratings for ' + cleanName)
+            return fig
+    return go.Figure()
 fig = go.Figure()
-#fig.show()
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -80,8 +83,8 @@ app.layout = html.Div(children=[
     html.H1("Breakdown of IMDb Ratings by Demographic"),
     html.Br(),
     dcc.Input(id="input1", type="text", placeholder="Type name of show", debounce=True),
-    html.Div(id="output"),
-    html.Div(id="output2"),
+    html.Button('Search', id='submit-val'),
+    html.Div(id="output1"),
     dcc.Graph(
         id='IMDb',
         figure=fig),
@@ -89,18 +92,16 @@ app.layout = html.Div(children=[
 )
 
 @app.callback(
-    Output('IMDb', 'figure'),
-    [Input("input1", "value")]
+    [Output('IMDb', 'figure'),
+    Output("output1", "children")],
+    [Input("submit-val", 'n_clicks')],
+    [State('input1', 'value')]
 )
-def update_output_fig(input_value):
-    return singleShow(input_value)
-
-@app.callback(
-    Output("output2", "children"),
-    [Input("input1", "value")]
-)
-def update_output_div(input_value):
-    return 'Output: {}'.format(input_value)
+def update_output(clicks, input_value):
+    updatedFig = singleShow(input_value)
+    if len(updatedFig['data']) > 0:
+        return updatedFig, ''
+    return updatedFig, 'No show found with name: {}'.format(input_value)
 
 if __name__ == '__main__':
     app.run_server()
